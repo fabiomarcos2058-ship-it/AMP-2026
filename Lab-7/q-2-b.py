18ccd37eccf86e00f203290e7f9ff065039381c8
fabiomarcos2058-ship-it <fabiomarcos2058@gmail.com>
2026-05-27 15:03:21 -0300

    Update q-2-b.py

--- Lab-7/q-2-b.py
+++ Lab-7/q-2-b.py
@@ -6,3 +6,5 @@ while contador < 10:
     soma += num
     contador += 1 
 print(f"A soma dos 10 números é: {soma}")
+#Explicação: O objetivo aqui é rodar um loop exatamente 10 vezes usando um contador. A cada volta, o código pede um número inteiro ao usuário 
+#e vai somando (acumulando) esse valor na variável 'soma', exibindo o total no final.
