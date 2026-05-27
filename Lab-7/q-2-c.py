796feace07ea7fd658e503976220265677c13b5e
fabiomarcos2058-ship-it <fabiomarcos2058@gmail.com>
2026-05-27 15:03:46 -0300

    Update q-2-c.py

--- Lab-7/q-2-c.py
+++ Lab-7/q-2-c.py
@@ -9,3 +9,7 @@ while contador < 10:
     contador += 1 
 
 print('O maior número é', maior)
+# Explicação: Este algoritmo descobre qual é o maior número de uma sequência. 
+# O truque é começar a variável 'maior' com menos infinito (float('-inf')), 
+# garantindo que o primeiro número digitado tome o posto de maior atual. 
+# A partir daí, se o próximo número digitado for maior, ele assume o trono.
