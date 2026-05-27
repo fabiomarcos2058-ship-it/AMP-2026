7882d2d560ddf7dfeff94195493528d982ac29cc
fabiomarcos2058-ship-it <fabiomarcos2058@gmail.com>
2026-05-27 15:04:26 -0300

    Update q-4.py

--- Lab-7/q-4.py
+++ Lab-7/q-4.py
@@ -14,3 +14,7 @@ while chances > 0:
 
 if chances == 0:
     print(f"Que pena, suas chances acabaram. O número era {numero_secreto}.")
+    # Explicação: Usando a biblioteca 'random', o código sorteia um número de 1 a 10. 
+# O usuário ganha 5 tentativas para acertar. Um loop controla as chances e um 'if' 
+# verifica se o palpite bateu em cheio com o número sorteado, usando o 'break' 
+# para dar a vitória na hora.
