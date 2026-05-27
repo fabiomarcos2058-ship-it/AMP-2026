ff0716284142affb633a85943552dc9fb77ed719
fabiomarcos2058-ship-it <fabiomarcos2058@gmail.com>
2026-05-27 15:00:49 -0300

    Update q-2-a.py

--- Lab-7/q-2-a.py
+++ Lab-7/q-2-a.py
@@ -6,7 +6,7 @@ while contador <= N:
     num = int(input("Digite um número: "))
     if num % 2 != 0:
         impares += 1
-    # CORREÇÃO: Faltava incrementar o contador para o loop não rodar para sempre
+    # Explicação: Faltava incrementar o contador para o loop não rodar para sempre
     contador += 1 
 
 print(f"Quantidade de ímpares: {impares}")
