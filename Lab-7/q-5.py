423405a22cae57dc794ea4f3bd6095b0e3b9befd
fabiomarcos2058-ship-it <fabiomarcos2058@gmail.com>
2026-05-27 15:04:46 -0300

    Update q-5.py

--- Lab-7/q-5.py
+++ Lab-7/q-5.py
@@ -18,3 +18,7 @@ while chances > 0:
 
 if chances == 0:
     print(f"Suas chances acabaram! O número secreto era {numero_secreto}.")
+    # Explicação: Uma evolução do jogo anterior. Agora o programa pega leve e dá 
+# dicas: usando a estrutura 'if/elif/else', ele compara o chute do usuário com 
+# o número sorteado e avisa se o número real é maior ou menor do que foi digitado, 
+# ajudando a fechar o cerco a cada erro.
